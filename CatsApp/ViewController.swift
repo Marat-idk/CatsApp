e871e28fb09d520e66d707f8b7f94b7632931546
Marat-idk <molodoy.dracon@gmail.com>
2023-02-08 22:16:38 +0300

    started project

---
+++ CatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsApp
+//
+//  Created by Marat on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
